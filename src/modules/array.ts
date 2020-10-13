cfbfb353efc87aa9fd720f0aa4f99d06a9c6e719
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-13 09:04:23 +0300

    fix bug

--- src/modules/array.ts
+++ src/modules/array.ts
@@ -1,6 +1,6 @@
 import * as number from "./number";
 
-async function random(array: Array<BufferSource>) {
+async function random(array: Array<any>) {
 	return array[await number.getRandomInt(array.length - 1, array.length)];
 }
 
