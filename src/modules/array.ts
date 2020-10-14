ec5d21dd85f663f1219f75988d50837a9e863d23
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-14 21:30:05 +0300

    fix bugs

--- src/modules/array.ts
+++ src/modules/array.ts
@@ -1,7 +1,7 @@
 import * as number from "./number";
 
 async function random(array: Array<any>) {
-	return array[await number.getRandomInt(0, array.length - 1)];
+	return array[await number.getRandomIntInclusive(0, array.length - 1)];
 }
 
 export { random };
