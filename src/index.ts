c6e3052dd3c19f83e05f883f44f58abe0afa1ca2
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-18 06:25:05 +0300

    add levenshtein

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as number from "./modules/number";
 import * as regular from "./modules/regular";
 import * as array from "./modules/array";
 import * as time from "./modules/time";
+import * as string from "./modules/string";
 
-export { logger, number, regular, array, time };
+export { logger, number, regular, array, time, string };
