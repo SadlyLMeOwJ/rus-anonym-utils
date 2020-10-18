a5466f83d755d6f12da176f6639d976e0c916e09
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-18 05:43:49 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as logger from "./modules/logger";
 import * as number from "./modules/number";
 import * as regular from "./modules/regular";
 import * as array from "./modules/array";
+import * as time from "./modules/time";
 
-export { logger, number, regular, array };
+export { logger, number, regular, array, time };
---
+++ src/modules/time.ts
@@ -0,0 +1,58 @@
+import { number } from "../index";
+
+export async function getTimeByMS(ms: number): Promise<string> {
+	let date = new Date(ms);
+	let hours: any = date.getHours();
+	let minutes: any = date.getMinutes();
+	let seconds: any = date.getSeconds();
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	return `${hours}:${minutes}:${seconds}`;
+}
+
+export async function getDateByMS(ms: number): Promise<string> {
+	let date = new Date(ms);
+	let days: any = date.getDate();
+	let month: any = date.getMonth() + 1;
+	let year: any = date.getFullYear();
+	if (month < 10) {
+		month = "0" + month;
+	}
+	if (days < 10) {
+		days = "0" + days;
+	}
+	return `${days}.${month}.${year}`;
+}
+
+export async function getDateTimeByMS(ms: number): Promise<string> {
+	let date = new Date(ms);
+	let hours: any = date.getHours();
+	let minutes: any = date.getMinutes();
+	let seconds: any = date.getSeconds();
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	let days: any = date.getDate();
+	let month: any = date.getMonth() + 1;
+	let year: any = date.getFullYear();
+	if (month < 10) {
+		month = "0" + month;
+	}
+	if (days < 10) {
+		days = "0" + days;
+	}
+	return `${hours}:${minutes}:${seconds} | ${days}.${month}.${year}`;
+}
