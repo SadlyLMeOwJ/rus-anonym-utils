e96369f2a75116d21d96b5a1d192e94c5b045450
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-04 02:01:16 +0300

    add number.separator

--- src/functions/number.ts
+++ src/functions/number.ts
@@ -22,10 +22,35 @@ function isInteger(number: number): boolean {
 	return (number ^ 0) === number;
 }
 
+function separator(number: number, separator: string): string {
+	let output = number.toString();
+	if (!output) {
+		throw new Error(`Invalid number`);
+	} else {
+		separator = separator || ".";
+		let splitted = output.split("");
+		let reversed = splitted.reverse();
+		let joined = reversed.join("");
+		let matched = joined.match(/[0-9]{1,3}/g);
+		let separated = matched?.join(
+			separator.toString().split("").reverse().join(""),
+		);
+		let splittedOutput = separated?.split("");
+		let reversedOutput = splittedOutput?.reverse();
+		let joinedOutput = reversedOutput?.join("");
+		if (joinedOutput) {
+			return joinedOutput;
+		} else {
+			throw new Error(`Invalid number`);
+		}
+	}
+}
+
 export {
 	getRandom,
 	getRandomArbitrary,
 	getRandomInt,
 	getRandomIntInclusive,
 	isInteger,
+	separator,
 };
