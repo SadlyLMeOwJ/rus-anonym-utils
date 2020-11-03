46a9d60ea68ddfa504099e9ed51ba906330c7264
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-03 20:08:53 +0300

    add vk.article.getByURL

--- src/functions/vk.ts
+++ src/functions/vk.ts
@@ -1,5 +1,6 @@
 import { VK } from "vk-io";
 import request from "request-promise";
+import { number, regular } from "..";
 
 /**
  * Получить идентификатор последней беседы в группе.
@@ -86,6 +87,49 @@ export const api = {
 			});
 		}
 		return outputArray;
+	},
+};
+
+export const article = {
+	getByURL: async (
+		articleLink: string,
+	): Promise<{
+		id: number;
+		owner_id: number;
+		access_hash: string;
+		title: string;
+		subtitle: string;
+		published: Date;
+		views: number;
+		shares: number;
+	}> => {
+		articleLink = articleLink.replace("https://m.", "https://");
+		if (regular.isURL(articleLink) === false) {
+			throw new Error(`Invalid article link`);
+		} else {
+			try {
+				let data = await request(articleLink);
+				let position1 = await data.indexOf(`Article.init({"id":`);
+				let position2 = await data.indexOf(`</script></div>`, position1);
+				data = data.substring(position1, position2);
+				position1 = 13;
+				position2 = await data.indexOf(`, {`);
+				data = data.substring(position1, position2);
+				let articleData = JSON.parse(data);
+				return {
+					id: articleData.id,
+					owner_id: articleData.owner_id,
+					access_hash: articleData.access_hash,
+					title: articleData.title,
+					subtitle: articleData.subtitle,
+					published: new Date(articleData.published_date * 1000),
+					views: articleData.views,
+					shares: articleData.shares,
+				};
+			} catch (error) {
+				throw new Error(`Invalid article`);
+			}
+		}
 	},
 };
 
