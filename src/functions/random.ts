f41dc3bccd2fd94783299b8eb73f4a7f9495bbb0
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-15 23:23:39 +0300

    add random methods

---
+++ src/functions/random.ts
@@ -0,0 +1,31 @@
+import * as array from "./array";
+
+function generatePercentsArray(
+	inputArray: Array<{
+		element: any;
+		possibility: number;
+	}>,
+): Array<any> {
+	let outputArray: Array<any> = [];
+	inputArray.map(function (element) {
+		for (let i = 0; i < element.possibility; i++) {
+			outputArray.push(element.element);
+		}
+	});
+	if (outputArray.length !== 100) {
+		throw new Error(`You must specify all 100%`);
+	} else {
+		return array.shuffle(outputArray);
+	}
+}
+
+function getRandomElementByPercents(
+	inputArray: Array<{
+		element: any;
+		possibility: number;
+	}>,
+): any {
+	return array.random(generatePercentsArray(inputArray));
+}
+
+export { generatePercentsArray, getRandomElementByPercents };
