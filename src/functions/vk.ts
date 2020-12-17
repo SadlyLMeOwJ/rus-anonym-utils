d600734d4e3a288b36a9c17d7b257015e6b79337
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-17 18:45:17 +0300

    add checkToken

--- src/functions/vk.ts
+++ src/functions/vk.ts
@@ -2,6 +2,150 @@ import { VK } from "vk-io";
 import { regular } from "../main";
 import axios from "axios";
 
+type accessRight =
+	| "notify"
+	| "friends"
+	| "photos"
+	| "audio"
+	| "video"
+	| "stories"
+	| "pages"
+	| "leftLink"
+	| "status"
+	| "notes"
+	| "messages"
+	| "wall"
+	| "ads"
+	| "offline"
+	| "docs"
+	| "groups"
+	| "notifications"
+	| "stats"
+	| "email"
+	| "market"
+	| "app_widget"
+	| "manage";
+
+const accessRights: {
+	user: Array<{
+		right: accessRight;
+		mask: number;
+	}>;
+	group: Array<{
+		right: accessRight;
+		mask: number;
+	}>;
+} = {
+	user: [
+		{
+			right: "notify",
+			mask: 1 << 0,
+		},
+		{
+			right: "friends",
+			mask: 1 << 1,
+		},
+		{
+			right: "photos",
+			mask: 1 << 2,
+		},
+		{
+			right: "audio",
+			mask: 1 << 3,
+		},
+		{
+			right: "video",
+			mask: 1 << 4,
+		},
+		{
+			right: "stories",
+			mask: 1 << 6,
+		},
+		{
+			right: "pages",
+			mask: 1 << 7,
+		},
+		{
+			right: "leftLink",
+			mask: 1 << 8,
+		},
+		{
+			right: "status",
+			mask: 1 << 10,
+		},
+		{
+			right: "notes",
+			mask: 1 << 11,
+		},
+		{
+			right: "messages",
+			mask: 1 << 12,
+		},
+		{
+			right: "wall",
+			mask: 1 << 13,
+		},
+		{
+			right: "ads",
+			mask: 1 << 15,
+		},
+		{
+			right: "offline",
+			mask: 1 << 16,
+		},
+		{
+			right: "docs",
+			mask: 1 << 17,
+		},
+		{
+			right: "groups",
+			mask: 1 << 18,
+		},
+		{
+			right: "notifications",
+			mask: 1 << 19,
+		},
+		{
+			right: "stats",
+			mask: 1 << 20,
+		},
+		{
+			right: "email",
+			mask: 1 << 22,
+		},
+		{
+			right: "market",
+			mask: 1 << 27,
+		},
+	],
+	group: [
+		{
+			right: "stories",
+			mask: 1 << 0,
+		},
+		{
+			right: "photos",
+			mask: 1 << 2,
+		},
+		{
+			right: "app_widget",
+			mask: 1 << 6,
+		},
+		{
+			right: "messages",
+			mask: 1 << 12,
+		},
+		{
+			right: "docs",
+			mask: 1 << 17,
+		},
+		{
+			right: "manage",
+			mask: 1 << 18,
+		},
+	],
+};
+
 /**
  * Секция для работы с методами требующими авторизацию от токена группы
  */
@@ -209,6 +353,84 @@ const article = {
 	},
 };
 
+async function checkToken(
+	token: string,
+): Promise<{
+	type: "user" | "group";
+	id: number;
+	accessRights: accessRight[];
+}> {
+	if (token.length !== 85) {
+		throw new Error("Invalid token length");
+	}
+
+	let splitToken = token.split("");
+	for (let tempWord of splitToken) {
+		let allowedWord = [
+			"d",
+			"e",
+			"f",
+			"b",
+			"c",
+			"a",
+			"0",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+		];
+		if (!allowedWord.find((x) => x === tempWord)) {
+			throw new Error("Invalid token symbols");
+		}
+	}
+
+	let tempVK = new VK({ token: token });
+	let tokenData = await tempVK.api.users.get({}).catch((err) => {
+		throw new Error("Invalid token");
+	});
+
+	let outputData: {
+		type: "user" | "group";
+		id: number;
+		accessRights: accessRight[];
+	} = {
+		type: "user",
+		id: 0,
+		accessRights: [],
+	};
+
+	if (tokenData.length === 0) {
+		let [secondTokenData] = await tempVK.api.groups.getById({});
+		outputData.type = "group";
+		outputData.id = secondTokenData.id;
+		let currentPermissions = await tempVK.api.groups.getTokenPermissions({});
+		for (let i in accessRights.group) {
+			if (
+				Boolean(currentPermissions.mask & accessRights.group[i].mask) === true
+			) {
+				outputData.accessRights.push(accessRights.group[i].right);
+			}
+		}
+	} else {
+		outputData.id = tokenData[0].id;
+		let currentPermissions = await tempVK.api.account.getAppPermissions({
+			user_id: outputData.id,
+		});
+		for (let i in accessRights.user) {
+			if (Boolean(currentPermissions & accessRights.user[i].mask) === true) {
+				outputData.accessRights.push(accessRights.user[i].right);
+			}
+		}
+	}
+
+	return outputData;
+}
+
 async function checkConversationID(
 	instanceVK: VK,
 	peerID: number,
@@ -233,4 +455,4 @@ async function checkConversationID(
  * @return класс с методами группы
  */
 
-export { group, user, api, article };
+export { group, user, api, article, checkToken, accessRights };
