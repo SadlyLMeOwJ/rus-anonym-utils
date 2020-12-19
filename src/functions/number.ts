59b6e48def7f5943cb159a83674b582be2d527f0
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-19 15:21:05 +0300

    add number description

--- src/functions/number.ts
+++ src/functions/number.ts
@@ -1,27 +1,60 @@
+/**
+ * Получение случайного числа от 0 (включительно) до 1 (не включая)
+ * @returns случайное числа от 0 (включительно) до 1 (не включая)
+ */
 function getRandom() {
 	return Math.random();
 }
 
+/**
+ * Получить псевдослучайное число с плавающей точкой в диапазоне от 0 до 1
+ * @param min - Минимальное значение
+ * @param max - Максимальное значение
+ * @returns Возвращает случайное число в заданном интервале. Возвращаемое значение не менее (и может быть равно) min и не более (и не равно) max.
+ */
 function getRandomArbitrary(min: number, max: number) {
 	return Math.random() * (max - min) + min;
 }
 
+/**
+ * Получение случайного целого числа в заданном интервале
+ * @param min - Минимальное значение
+ * @param max - Максимальное значение
+ * @returns Возвращает случайное целое число в заданном интервале. Возвращаемое значение не менее min (или следующее целое число, которое больше min, если min не целое) и не более (но не равно) max.
+ */
 function getRandomInt(min: number, max: number) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/**
+ * Получение случайного целого числа в заданном интервале, включительно
+ * @param min - Минимальное значение
+ * @param max - Максимальное значение
+ * @returns Возвращает случайное целое число в заданном интервале. Возвращаемое значение не менее min (или следующее целое число, которое больше min, если min не целое) и не более (но не равно) max включительно
+ */
 function getRandomIntInclusive(min: number, max: number) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Проверяет, является ли число целым
+ * @param number - Число
+ * @returns Возвращает true, если число целое, и false если оно не является целым
+ */
 function isInteger(number: number): boolean {
 	return (number ^ 0) === number;
 }
 
+/**
+ * Разделяет число по 3 символа
+ * @param number - Число
+ * @param separator - Разделитель
+ * @returns Итоговую строку
+ */
 function separator(number: number, separator: string): string {
 	let output = number.toString();
 	if (!output) {
