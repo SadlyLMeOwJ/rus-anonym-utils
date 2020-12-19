51eecd038136c633590ea16ab24ddf47133ff637
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-19 07:46:55 +0300

    add basic logic operations

--- src/functions/logical.ts
+++ src/functions/logical.ts
@@ -1,5 +1,57 @@
 /**
+ * Логическое И
+ * Конъюнкция
+ * Логическое умножение, выражение «AND».
+ * Конъюнкция возвращает true только тогда, когда оба аргумента равны true, иначе false.
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function AND(a: boolean, b: boolean): boolean {
+	return a && b;
+}
+
+/**
  * Логическое ИЛИ
+ * Дизъюнкция
+ * Логическое сложение, выражение «OR». Также называется «слабой дизъюнкцией».
+ * Возвращает false только тогда, когда оба аргумента равны false, иначе true.
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function OR(a: boolean, b: boolean): boolean {
+	return a || b;
+}
+
+/**
+ * Логическое И-НЕ
+ * Возвращает true только тогда, когда оба аргумента равны true, иначе false
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function ANOT(a: boolean, b: boolean): boolean {
+	return a === true && b === true;
+}
+
+/**
+ * Логическое ИЛИ-НЕ
+ * Иначе стрелка Пирса, выражение «NOR».
+ * Возвращает true только тогда, когда оба аргумента одновременно равны false.
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function NOR(a: boolean, b: boolean): boolean {
+	return a === false && b === false;
+}
+
+/**
+ * Логическое исключающее ИЛИ
+ * Сильная дизъюнкция
+ * Исключающая дизъюнкция, выражение «XOR».
+ * Возвращает true только тогда, когда только один из них равен true, а второй равен false.
  * @param {boolean} a - 1 значение
  * @param {boolean} b - 2 значение
  * @return {boolean} - результат
@@ -8,4 +60,41 @@ function XOR(a: boolean, b: boolean): boolean {
 	return a ? !b : b;
 }
 
-export { XOR };
+/**
+ * Логическое НЕ
+ * Отрицание
+ * Инверсия, негация, выражение «NOT».
+ * Возвращает противоположное значение: для false — true, для true — false.
+ * @param {boolean} a - 1 значение
+ * @return {boolean} - результат
+ */
+function NOT(a: boolean): boolean {
+	return !a;
+}
+
+/**
+ * Логическое РАВНО
+ * Эквивалентность
+ * Тождество, равенство, выражение «EQ».
+ * Возвращает true только тогда, когда оба аргумента равны одновременно false или true.
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function EQ(a: boolean, b: boolean): boolean {
+	return a === b;
+}
+
+/**
+ * Импликация
+ * Выражает зависимость причины и следствия.
+ * То есть возвращает ложь только тогда, когда первый аргумент равен true, а второй аргумент — false.
+ * @param {boolean} a - 1 значение
+ * @param {boolean} b - 2 значение
+ * @return {boolean} - результат
+ */
+function IMP(a: boolean, b: boolean): boolean {
+	return a === true && b === false ? false : true;
+}
+
+export { AND, OR, XOR, NOT, NOR, IMP };
