e8283074da23dbd68baba23587c35f2444115887
rus_anonym <alexandrsemin2033@gmail.com>
2020-12-19 15:06:39 +0300

    update doc for npm.ts

--- src/functions/npm.ts
+++ src/functions/npm.ts
@@ -2,47 +2,45 @@
 import instanceNPM from "npm-api";
 const npm = new instanceNPM();
 
-interface checkDependenciesResponse {
-	name: string;
-	lastVersion: string;
-	currentVersion: string;
-	isOutdated: boolean;
+/**
+ * Получает массив с списком всех модулей
+ * @param packageJSON - package.json
+ * @returns массив с всеми модулями
+ */
+async function getDependecies(
+	packageJSON: any,
+): Promise<
+	Array<{
+		name: string;
+		lastVersion: string;
+		currentVersion: string;
+		isOutdated: boolean;
+	}>
+> {
+	let arrayWithDependecies = [];
+	for (let i in packageJSON.dependencies) {
+		const currentDependence = await npm.repo(i);
+		const currentDependenceVersion = await getLatestVersion(i);
+		const oldDependenceVersion = packageJSON.dependencies[i].replace("^", "");
+		arrayWithDependecies.push({
+			name: currentDependence.name,
+			lastVersion: currentDependenceVersion,
+			currentVersion: oldDependenceVersion,
+			isOutdated: currentDependenceVersion !== oldDependenceVersion,
+		});
+	}
+	return arrayWithDependecies;
 }
 
-class packageJSON {
-	private packageJSON: any;
-	constructor(packageJSON: any) {
-		this.packageJSON = packageJSON;
-	}
-
-	async checkDependencies(): Promise<Array<checkDependenciesResponse>> {
-		if (!this.packageJSON.dependencies) {
-			return [];
-		} else {
-			let arrayWithDependecies = [];
-			for (let i in this.packageJSON.dependencies) {
-				const currentDependence = await npm.repo(i);
-				const currentDependenceVersion = await getLatestVersion(i);
-				const oldDependenceVersion = this.packageJSON.dependencies[i].replace(
-					"^",
-					"",
-				);
-				arrayWithDependecies.push({
-					name: currentDependence.name,
-					lastVersion: currentDependenceVersion,
-					currentVersion: oldDependenceVersion,
-					isOutdated: currentDependenceVersion !== oldDependenceVersion,
-				});
-			}
-			return arrayWithDependecies;
-		}
-	}
-}
-
-async function getLatestVersion(moduleName: string) {
+/**
+ * Получает последнюю версию переданного модуля
+ * @param moduleName - название модуля
+ * @returns Последняя версия модуля
+ */
+async function getLatestVersion(moduleName: string): Promise<string> {
 	return (
 		await (await npm.repo(moduleName)).version(`latest`)
 	).version.toString();
 }
 
-export { packageJSON, getLatestVersion };
+export { getDependecies, getLatestVersion };
