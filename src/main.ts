8f0ad32fdf1f131a213eebe3e06efb4cab41dc49
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-01 12:50:45 +0300

    rename dir

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@
  * @module Utils
  */
 
-import * as array from "./functions/array";
-import * as logger from "./functions/logger";
-import * as logical from "./functions/logical";
-import * as number from "./functions/number";
-import * as regular from "./functions/regular";
-import * as smileys from "./functions/smileys";
-import * as string from "./functions/string";
-import * as time from "./functions/time";
-import * as vk from "./functions/vk";
+import * as array from "./lib/array";
+import * as logger from "./lib/logger";
+import * as logical from "./lib/logical";
+import * as number from "./lib/number";
+import * as regular from "./lib/regular";
+import * as smileys from "./lib/smileys";
+import * as string from "./lib/string";
+import * as time from "./lib/time";
+import * as vk from "./lib/vk";
 
 export { array, logger, logical, number, regular, smileys, string, time, vk };
