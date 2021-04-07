c0dbabd198b6786c2a25d11835d67ec0494b2a3e
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-07 03:19:53 +0300

    update method

--- src/lib/regular/core.ts
+++ src/lib/regular/core.ts
@@ -82,7 +82,7 @@ export class RegularUtils {
 	 * @param email {string} - E-Mail адрес
 	 * @returns {boolean}
 	 */
-	public isNumber(number: string): boolean {
+	public isPhoneNumber(number: string): boolean {
 		return new RegExp(regularList.number, "i").test(number);
 	}
 
