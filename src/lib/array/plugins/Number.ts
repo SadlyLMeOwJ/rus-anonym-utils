73fbd78b9b68336ad3ed93d74ed243947611ee32
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-29 22:13:45 +0300

    add example for array

--- src/lib/array/plugins/Number.ts
+++ src/lib/array/plugins/Number.ts
@@ -20,6 +20,10 @@ class NumberArray {
 	 * @description Вывод минимального значения в массиве
 	 * @param {Array.<number>} inputArray Массив с числами
 	 * @returns {number} Минимальное значение
+	 *
+	 * @example
+	 * // Return -50
+	 * array.number.min([1, 2, 3, 4, 5, 6, -50]);
 	 */
 	public min(inputArray: number[]): number {
 		return Math.min.apply(null, inputArray);
@@ -29,6 +33,10 @@ class NumberArray {
 	 * @description Вывод максимального значения в массиве
 	 * @param {Array.<number>} inputArray массив с числами
 	 * @returns {number} максимальное значение
+	 *
+	 * @example
+	 * // Return 6
+	 * array.number.min([1, 2, 3, 4, 5, 6, -50]);
 	 */
 	public max(inputArray: number[]): number {
 		return Math.max.apply(null, inputArray);
@@ -38,6 +46,10 @@ class NumberArray {
 	 * @description Вывод среднего значения в массиве
 	 * @param {Array.<number>} inputArray массив с числами
 	 * @returns {number} среднее значение
+	 *
+	 * @example
+	 * // Return 3
+	 * array.number.average([1, 2, 3, 4, 5]);
 	 */
 	public average(inputArray: number[]): number {
 		return inputArray.reduce((a, b) => a + b) / inputArray.length;
@@ -46,7 +58,11 @@ class NumberArray {
 	/**
 	 * @description Подсчёт суммы массива
 	 * @param {Array.<number>} inputArray массив с числами
-	 * @returns {number} среднее значение
+	 * @returns {number} сумма массива
+	 *
+	 * @example
+	 * // Return 15
+	 * array.number.total([1, 2, 3, 4, 5]);
 	 */
 	public total(inputArray: number[]): number {
 		return inputArray.reduce(
@@ -59,7 +75,20 @@ class NumberArray {
 	 * @description Генерирует массив заполненный выбранной цифрой, либо нулями
 	 * @param {number} length - Длина выходного массива
 	 * @param {number} customNumber - Цифра, которой нужно заполнить массив, или функция её генерирующая
-	 * @returns {Array.<number>} - Массив заполнный нулями
+	 * @returns {Array.<number>} - Массив
+	 *
+	 * @example
+	 * // Return [0, 0, 0, 0, 0]
+	 * array.number.generate(5);
+	 *
+	 * @example
+	 * // Return [5, 5, 5, 5, 5]
+	 * array.number.generate(5, 5);
+	 *
+	 * @example
+	 * // Return [1, 2, 3, 4, 5]
+	 * let i = 1;
+	 * array.number.generate(5, () => ++i);
 	 */
 	public generate(
 		length: number,
--- src/lib/array/plugins/NumberSort.ts
+++ src/lib/array/plugins/NumberSort.ts
@@ -1,3 +1,5 @@
+/* eslint-disable jsdoc/require-example */
+
 import {
 	SortingBenchmarkResponse,
 	sortingAlgorithm,
