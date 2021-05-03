97d29823e8706f658851fb4facaefc1ec09b5831
RusAnonym <alexandrsemin2033@gmail.com>
2021-05-03 13:36:01 +0300

    add tests for utils.sleep

--- tests/main.ts
+++ tests/main.ts
@@ -1,6 +1,21 @@
+import utils from "../dist/main";
+import assert from "assert";
+import { describe, it } from "mocha";
+
 import "./array/main";
 import "./logical";
 import "./number";
 import "./regular";
 import "./string";
 import "./IP";
+
+describe("Main", function () {
+	it("sleep", async () => {
+		let i = 0;
+		setTimeout(() => {
+			assert.strictEqual(i, 0);
+		}, 25);
+		await utils.sleep(50);
+		i = 5;
+	});
+});
