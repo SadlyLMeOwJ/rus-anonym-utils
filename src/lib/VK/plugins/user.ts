f494b753e712c925aaf5072cbaaea5f76e1924b5
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-28 16:22:10 +0300

    add getUserRegDate method & getUserModifiedDate method

--- src/lib/VK/plugins/user.ts
+++ src/lib/VK/plugins/user.ts
@@ -1,5 +1,6 @@
 /* eslint-disable jsdoc/require-example */
 
+import axios from "axios";
 import moment from "moment";
 import { VK } from "vk-io";
 import { StoreGetProductsResponse } from "vk-io/lib/api/schemas/responses";
@@ -292,6 +293,30 @@ export class VK_User {
             steps,
             distance,
         });
+    }
+
+    /**
+     * @description Позволяет узнать дату регистрации пользователя
+     * @param {number} id - ID пользователя
+     * @returns {Date} - Дата регистрации пользователя
+     */
+    public async getUserRegDate(id: number): Promise<Date> {
+        const data = (await axios.get(`http://vk.com/foaf.php?id=${id}`)).data;
+        const firstIndex = data.indexOf(`<ya:created dc:date="`) + 21;
+        const secondIndex = data.indexOf(`"/>`, firstIndex);
+        return moment(data.substring(firstIndex, secondIndex)).toDate();
+    }
+
+    /**
+     * @description Позволяет узнать дату изменения информации пользователя
+     * @param {number} id - ID пользователя
+     * @returns {Date} - Дата изменения страницы пользователя
+     */
+    public async getUserModifiedDate(id: number): Promise<Date> {
+        const data = (await axios.get(`http://vk.com/foaf.php?id=${id}`)).data;
+        const firstIndex = data.indexOf(`<ya:modified dc:date="`) + 22;
+        const secondIndex = data.indexOf(`"/>`, firstIndex);
+        return moment(data.substring(firstIndex, secondIndex)).toDate();
     }
 }
 
